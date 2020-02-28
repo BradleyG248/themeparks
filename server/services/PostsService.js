da26ce24bc582f056719cb014fdc8b678191b1a0
Jake-Fullmer <jnfu@outlook.com>
2020-02-28 10:59:39 -0700

    posts get route complete

--- server/services/PostsService.js
+++ server/services/PostsService.js
@@ -1,6 +1,8 @@
 import { dbContext } from "../db/DbContext";
 import { BadRequest } from "../utils/Errors";
 
+
+
 class PostsService {
   async getAll(query = {}) {
     let posts = await dbContext.Post.find(query).populate(
