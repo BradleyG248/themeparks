706723a8bbab226001c8395d3b057e3d2ad79d6d
Jake-Fullmer <jnfu@outlook.com>
2020-02-28 16:27:54 -0700

    edit done

--- server/controllers/PostsController.js
+++ server/controllers/PostsController.js
@@ -5,16 +5,17 @@ import auth0Provider from "@bcwdev/auth0Provider";
 
 
 export class PostsController extends BaseController {
+
   constructor() {
     super("api/posts");
     this.router = express
       .Router()
       .get("", this.getAll)
-      // .get("/:id", this.getById)
+      .get("/:id", this.getById)
       // NOTE: Beyond this point all routes require Authorization tokens (the user must be logged in)
       .use(auth0Provider.getAuthorizedUserInfo)
       .post("", this.create)
-    // .put("/:id", this.edit)
+      .put("/:id", this.edit);
     // .delete("/:id", this.delete);
   }
   async getAll(req, res, next) {
@@ -25,11 +26,27 @@ export class PostsController extends BaseController {
       next(error);
     }
   }
+  async getById(req, res, next) {
+    try {
+      let data = await postsService.getById(req.params.id);
+      res.status(201).send(data);
+    } catch (error) {
+      next(error);
+    }
+  }
   async create(req, res, next) {
     try {
       req.body.creatorEmail = req.userInfo.email;
       let post = await postsService.create(req.body);
       res.send(post);
+    } catch (error) {
+      next(error);
+    }
+  }
+  async edit(req, res, next) {
+    try {
+      let data = await postsService.edit(req.params.id, req.body, req.userInfo.email);
+      res.send(data);
     } catch (error) {
       next(error);
     }
--- server/services/PostsService.js
+++ server/services/PostsService.js
@@ -4,7 +4,6 @@ import { BadRequest, UnAuthorized } from "../utils/Errors";
 
 
 class PostsService {
-
   async getAll(query = {}) {
     let posts = await dbContext.Post.find(query).populate(
       "creator",
@@ -12,11 +11,33 @@ class PostsService {
     );
     return posts;
   }
+  async getById(id) {
+    return await dbContext.Post.findById(id);
+  }
   async create(body, email) {
     return await dbContext.Post.create(body);
 
   }
-
+  // async edit(id, body, email) {
+  //   let post = await dbContext.Post.findById(id);
+  //   // if (post["creator"] != email) {
+  //   //   throw new UnAuthorized();
+  //   // }
+  //   post = body;
+  //   await post.save();
+  //   return post;
+  // }
+  async edit(id, update, email) {
+    let post = await dbContext.Post.findByIdAndUpdate(id, update);
+    // @ts-ignore
+    if (post.creator != email) {
+      throw new UnAuthorized();
+    }
+    // @ts-ignore
+    post.body = update.body;
+    await post.save();
+    return post;
+  }
 }
 
 export const postsService = new PostsService();
