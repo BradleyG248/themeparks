1adf5c54189137a4b09d2ced923e176067b0ced0
BradleyG248 <calvinandhobbs28@gmail.com>
2020-04-21 13:14:50 -0600

    asdf

--- clientsrc/src/components/addPosts.vue
+++ clientsrc/src/components/addPosts.vue
@@ -1,27 +1,48 @@
 <template>
   <div class="component">
-    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#post-creator">create post</button>
-    <div class="modal" tabindex="-1" role="dialog" id="post-creator" @submit.prevent="createPost">
+    <button
+      type="button"
+      class="btn btn-primary"
+      data-toggle="modal"
+      data-target="#post-creator"
+    >create post</button>
+    <div class="modal" tabindex="10" role="dialog" id="post-creator" @submit.prevent="createPost">
       <div class="modal-dialog" role="document">
-        <form action="">
-        <div class="modal-content form-group rounded input-group-default">
-          <div class="modal-header d-flex flex-column">
-            <h2>Title</h2>
-            <input class="modal-title rounded form-control" placeholder="Hyped?" aria-describedby="inputGroup-sizing-default" v-model="newPost.title"/>
+        <form action>
+          <div class="modal-content form-group rounded input-group-default">
+            <div class="modal-header d-flex flex-column">
+              <h2>Title</h2>
+              <input
+                class="modal-title rounded form-control"
+                placeholder="Hyped?"
+                aria-describedby="inputGroup-sizing-default"
+                v-model="newPost.title"
+              />
+            </div>
+            <div class="modal-body">
+              <h3>Description</h3>
+              <input
+                class="rounded form-control"
+                placeholder="Elaborate..."
+                v-model="newPost.description"
+              />
+              <h3>Image</h3>
+              <input
+                class="rounded form-control"
+                placeholder="Hands in the air like you just don't care"
+                v-model="newPost.imgUrl"
+              />
+            </div>
+            <div class="modal-footer">
+              <button
+                type="submit"
+                class="btn btn-primary"
+                data-toggle="modal"
+                data-target="#post-creator"
+              >Submit</button>
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
           </div>
-          <div class="modal-body">
-            <h3>Description</h3>
-            <input class="rounded form-control" placeholder="Elaborate..." v-model="newPost.description"/>
-          </div>
-          <div class="modal-body">
-            <h3>Image</h3>
-            <input class="rounded form-control" placeholder="Hands in the air like you just don't care" v-model="newPost.imgUrl" />
-          </div>
-          <div class="modal-footer">
-            <button type="submit" class="btn btn-primary" data-toggle="modal" data-target="#post-creator">Submit</button>
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-          </div>
-        </div>
         </form>
       </div>
     </div>
@@ -31,30 +52,28 @@
 
 <script>
 export default {
-  name: 'AddPost',
-  data(){
+  name: "AddPost",
+  data() {
     return {
-      newPost:{}
+      newPost: {}
+    };
+  },
+  computed: {},
+  methods: {
+    createPost() {
+      this.$store.dispatch("createPost", this.newPost);
     }
   },
-  computed:{
-
-  },
-  methods:{
-    createPost(){
-      this.$store.dispatch("createPost", this.newPost)
-    }
-  },
-  components:{
-
-  }
-}
+  components: {}
+};
 </script>
 
 
 <style scoped>
-div.modal-content{
-  background-color: #F7F4F3;
-  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif
+div.modal-content {
+  z-index: 10;
+  background-color: #f7f4f3;
+  font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande",
+    "Lucida Sans", Arial, sans-serif;
 }
 </style>
