8e4de3797ae86ad5e7a3bb23295d4c37877513e4
BradleyG248 <calvinandhobbs28@gmail.com>
2020-04-23 11:19:07 -0600

    auth

--- clientsrc/src/authConfig.js
+++ clientsrc/src/authConfig.js
@@ -1,3 +1,3 @@
-export const domain = "jnfu.auth0.com";
-export const audience = "https://jnfu-server.com";
-export const clientId = "gWcHY8x41Gkzwbo8T8PoFgnauxsWGXlm";
+export const domain = "ten-1085.auth0.com";
+export const audience = "bradleyslibrary.com";
+export const clientId = "xaMXmVh3aIuG72q0WNDsoO2LP962x4Qk";
