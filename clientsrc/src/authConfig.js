c7378f7f2b232e2f19d25e8c8bd1b9965fc973b4
BradleyG248 <calvinandhobbs28@gmail.com>
2020-04-23 15:18:43 -0600

    built

--- clientsrc/src/authConfig.js
+++ clientsrc/src/authConfig.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export const domain = "ten-1085.auth0.com";
 export const audience = "bradleyslibrary.com";
 export const clientId = "xaMXmVh3aIuG72q0WNDsoO2LP962x4Qk";
@@ -6,8 +5,3 @@ export const clientId = "xaMXmVh3aIuG72q0WNDsoO2LP962x4Qk";
 // export const domain = "jnfu.auth0.com";
 // export const audience = "https://jnfu-server.com";
 // export const clientId = "gWcHY8x41Gkzwbo8T8PoFgnauxsWGXlm";
-=======
-export const domain = "jnfu.auth0.com";
-export const audience = "https://jnfu-server.com";
-export const clientId = "gWcHY8x41Gkzwbo8T8PoFgnauxsWGXlm";
->>>>>>> 30d1b514f0521c5a790f867d59300283634077b5
