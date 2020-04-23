30d1b514f0521c5a790f867d59300283634077b5
jhelm3 <jakehelm36@gmail.com>
2020-04-23 15:00:19 -0600

    auth0 fixes

--- clientsrc/src/authConfig.js
+++ clientsrc/src/authConfig.js
@@ -1,3 +1,3 @@
 export const domain = "jnfu.auth0.com";
-export const audience = "jnfu-server.com";
+export const audience = "https://jnfu-server.com";
 export const clientId = "gWcHY8x41Gkzwbo8T8PoFgnauxsWGXlm";
